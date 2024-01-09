268cb62ea3ff90c5efa4b5495b283b7bd963d571
Christian <ferchris82@hotmail.com>
2024-01-09 13:56:44 -0500

    Versión 0.1

---
+++ src/org/chrisferdev/api/stream/ejemplos/EjemploStreamMap.java
@@ -0,0 +1,21 @@
+package org.chrisferdev.api.stream.ejemplos;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class EjemploStreamMap {
+    public static void main(String[] args) {
+
+        Stream<String> nombres = Stream
+                .of("Pato", "Paco", "Pepa", "Pepe")
+                .map(String::toUpperCase)
+                .peek(System.out::println)
+                .map(String::toLowerCase);
+
+        List<String> lista = nombres.collect(Collectors.toList());
+        lista.forEach(System.out::println);
+        //nombres.forEach(System.out::println);
+    }
+}
