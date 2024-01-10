4c4f544a5db1a7e1067cb1ab8ed3f1f110fe96e5
Christian <ferchris82@hotmail.com>
2024-01-10 13:13:06 -0500

    Versión 1.0

---
+++ src/org/chrisferdev/api/stream/ejemplos/EjemploStreamFilterSingle2.java
@@ -0,0 +1,20 @@
+package org.chrisferdev.api.stream.ejemplos;
+
+import org.chrisferdev.api.stream.ejemplos.models.Usuario;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class EjemploStreamFilterSingle2 {
+    public static void main(String[] args) {
+
+        Usuario usuario = Stream
+                .of("Pato Guzman", "Paco Gonzalez", "Pepa Gutierrez", "Pepe Mena",
+                        "Pepe Garcia")
+                .map(nombre -> new Usuario(nombre.split(" ")[0], nombre.split(" ")[1]))
+                .filter(u -> u.getId().equals(5))
+                .findFirst().get();
+
+        System.out.println(usuario);
+    }
+}
