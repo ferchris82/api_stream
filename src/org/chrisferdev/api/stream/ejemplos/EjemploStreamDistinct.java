2b1c3c5beef1ca0bffe2e53a0d8a14539ea60442
Christian <ferchris82@hotmail.com>
2024-01-10 14:09:51 -0500

    Versión 1.0

---
+++ src/org/chrisferdev/api/stream/ejemplos/EjemploStreamDistinct.java
@@ -0,0 +1,18 @@
+package org.chrisferdev.api.stream.ejemplos;
+
+import org.chrisferdev.api.stream.ejemplos.models.Usuario;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class EjemploStreamDistinct {
+    public static void main(String[] args) {
+
+        Stream<String> nombres = Stream.of("Pato Guzman", "Paco Gonzalez", "Pepa Gutierrez", "Pepe Mena",
+                        "Pepe Garcia", "Paco Gonzalez", "Paco Gonzalez", "Paco Gonzalez")
+                .distinct();
+
+        nombres.forEach(System.out::println);
+
+    }
+}
---
+++ src/org/chrisferdev/api/stream/ejemplos/EjemploStreamFilterEmpty.java
@@ -0,0 +1,20 @@
+package org.chrisferdev.api.stream.ejemplos;
+
+import org.chrisferdev.api.stream.ejemplos.models.Usuario;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class EjemploStreamFilterEmpty {
+    public static void main(String[] args) {
+
+        long count = Stream
+                .of("Pato Guzman", "Paco Gonzalez", "", "Pepe Mena",
+                        "")
+                .filter(String::isEmpty)
+                .peek(System.out::println)
+                .count();
+        System.out.println("count = " + count);
+
+    }
+}
