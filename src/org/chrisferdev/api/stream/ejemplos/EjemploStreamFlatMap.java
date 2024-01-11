fd8e320f3044a0eb5c84f0ca90edbeb3cf7f96a5
Christian <ferchris82@hotmail.com>
2024-01-10 21:36:20 -0500

    Versión 1.0

--- src/org/chrisferdev/api/stream/ejemplos/EjemploStreamFlatMap.java
+++ src/org/chrisferdev/api/stream/ejemplos/EjemploStreamFlatMap.java
@@ -2,22 +2,24 @@ package org.chrisferdev.api.stream.ejemplos;
 
 import org.chrisferdev.api.stream.ejemplos.models.Usuario;
 
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class EjemploStreamFilter {
+public class EjemploStreamFlatMap {
     public static void main(String[] args) {
 
         Stream<Usuario> nombres = Stream
                 .of("Pato Guzman", "Paco Gonzalez", "Pepa Gutierrez", "Pepe Mena",
                         "Pepe Garcia")
                 .map(nombre -> new Usuario(nombre.split(" ")[0], nombre.split(" ")[1]))
-                .filter(u -> u.getNombre().equals("Pepe"))
+                .flatMap(u -> {
+                    if(u.getNombre().equalsIgnoreCase("Pepe")){
+                        return Stream.of(u);
+                    }
+                    return Stream.empty();
+                })
                 .peek(System.out::println);
 
-        List<Usuario> lista = nombres.collect(Collectors.toList());
-        lista.forEach(System.out::println);
         //nombres.forEach(System.out::println);
+        System.out.println(nombres.count());
     }
 }
